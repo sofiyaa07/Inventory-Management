eada38bd9f25acd18f2a97e9efac171a75163504
Helene Li <lih20600@gmail.com>
2025-05-13 10:05:52 -0400

    Added helper method to append object info to CSV

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Import Part class and helper functions
 import { readFile } from 'fs/promises';
 import * as helpers from './helper-methods.js';
+import Part from './part.js';
 
 // Make shelf
 let shelf1 = helpers.makeShelf(3,4);
 
-// Read in CSV file
+// Read in CSV file (CSV -> str -> obj)
 const csvPath = './Part Database - Sheet1.csv';
-
 try {
     const csvString = await readFile(csvPath, 'utf8');
     console.log('CSV read.');
@@ -15,11 +15,20 @@ try {
     // Separate CSV string into key value pairs 
     const parts = helpers.csvToObjects(csvString);
 
-    console.log(parts);
     // Sort objects by name, alphabetical 
-    // let partAlpha = helpers.sortAlphaAsc(objects);
+    // let partAlpha = helpers.sortAlphaAsc(parts);
     // console.log(partAlpha);
 
 } catch (err) {
     console.error('Error reading file: ', err);
 }
+
+// Write to CSV file (obj -> str -> CSV)
+let part0 = new Part("Arduino Nano 33 BLE Rev2 with headers", "ABX00072", shelf1[1][2], 35, "note", "https...", "image-location.jpg", 25);
+// Get Object values only, no keys
+part0 = Object.values(part0).map(value => value.toString());
+// Convert from Object to String
+let part0Str = JSON.stringify(part0);
+// Get rid of quotes and square brackets before writing to CSV
+part0Str = part0Str.replace(/["\[\]]/g, '');
+helpers.writeToCSV('./Part Database - Sheet1.csv', part0Str);
