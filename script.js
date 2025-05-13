ba6b8f45dd52fc2ab90ea98e4a793f21b8c86666
Helene Li <lih20600@gmail.com>
2025-05-13 10:38:05 -0400

    cleaned up script by adding helper function

--- script.js
+++ script.js
@@ -15,20 +15,10 @@ try {
     // Separate CSV string into key value pairs 
     const parts = helpers.csvToObjects(csvString);
 
-    // Sort objects by name, alphabetical 
-    // let partAlpha = helpers.sortAlphaAsc(parts);
-    // console.log(partAlpha);
-
 } catch (err) {
     console.error('Error reading file: ', err);
 }
 
 // Write to CSV file (obj -> str -> CSV)
 let part0 = new Part("Arduino Nano 33 BLE Rev2 with headers", "ABX00072", shelf1[1][2], 35, "note", "https...", "image-location.jpg", 25);
-// Get Object values only, no keys
-part0 = Object.values(part0).map(value => value.toString());
-// Convert from Object to String
-let part0Str = JSON.stringify(part0);
-// Get rid of quotes and square brackets before writing to CSV
-part0Str = part0Str.replace(/["\[\]]/g, '');
-helpers.writeToCSV('./Part Database - Sheet1.csv', part0Str);
+helpers.addObjectInfoToCSV(part0, './Part Database - Sheet1.csv');
